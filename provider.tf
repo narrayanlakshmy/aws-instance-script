1dd35f095e3aaf40ed7fd66a32fda78db5d256f4
narrayanlakshmy <108192464+narrayanlakshmy@users.noreply.github.com>
2022-09-21 18:44:44 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,10 @@
 provider "aws" {
   access_key = "AKIA6M4VVQ33OXKARHXK"
   secret_key = "ra1ITIjv5wS2Q1EQspzO10Z9asGmJWEjdrorjO3j"
-  
-  region = "us-east-1"
+    region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami     = "ami-2757f631"
+  instance_type = "t2.micro"
 }
