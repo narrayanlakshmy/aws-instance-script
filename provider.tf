289e66a09b9a5614ebeef6678e5a3607dd67bb3f
narrayanlakshmy <108192464+narrayanlakshmy@users.noreply.github.com>
2022-09-22 11:47:56 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,28 @@
 provider "aws" {
-  access_key = "AKIA6M4VVQ33OXKARHXK"
-  secret_key = "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"
-    region = "us-east-1"
+  access_key = "AKIA6M4VVQ33ATAYVIBJ"
+  secret_key = "vS5As0eFL9vE0sk/VIgYx5z/+UvN18gBRu6SF5lF"
+    region = "ap-northeast-1"
 }
 
-resource "aws_instance" "example" {
-  ami     = "ami-2757f631"
+resource "aws_instance" "firstec2" {
+  ami     = "ami-0f36dcfcc94112ea1"
   instance_type = "t2.micro"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
 }
