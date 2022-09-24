0baed763350d4d25ab991474a28796d0f4a7a814
narrayanlakshmy <108192464+narrayanlakshmy@users.noreply.github.com>
2022-09-24 11:58:57 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -5,3 +5,11 @@ variable "AWS_ACCESS_KEY_ID" {
 variable "AWS_SECRET_ACCESS_KEY"{
   default = "LurV66s7ydEGsYdh+CKQAYSGCYdp6D6PwXm2yVfF"
 }
+
+variable "vpc_cidr"{
+  default = "10.0.0.0/16"
+}
+
+variable "var.tenancy"{
+  default = "default"
+}
