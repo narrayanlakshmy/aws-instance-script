04b79e0ab4ca2ea875efd509d630fcc0ac79b726
narrayanlakshmy <108192464+narrayanlakshmy@users.noreply.github.com>
2022-09-24 11:51:22 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,7 +1,7 @@
 variable "AWS_ACCESS_KEY_ID" {
-  default = "AKIA6M4VVQ33IGC3XWXG"
+  default = "AKIA6M4VVQ33EZHKC3JH"
 }
 
 variable "AWS_SECRET_ACCESS_KEY"{
-  default = "DzrS2emfJfoILeIajOnZbD/9C2SpjcUZ3nariOc0"
+  default = "LurV66s7ydEGsYdh+CKQAYSGCYdp6D6PwXm2yVfF"
 }
