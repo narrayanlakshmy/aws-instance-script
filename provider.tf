16158c0c57a1d54eae1b9ac2cddf481e4252a5f1
narrayanlakshmy <108192464+narrayanlakshmy@users.noreply.github.com>
2022-09-24 11:59:03 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,8 +10,8 @@ resource "aws_instance" "firstec2" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+  cidr_block       = "${var.vpc_cidr}"
+  instance_tenancy = "${var.tenancy}"
 
   tags = {
     Name = "main"
