bfaf1d21b81ce966d967ca6b9c9f61e9b144ecb3
narrayanlakshmy <108192464+narrayanlakshmy@users.noreply.github.com>
2022-09-26 11:03:15 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,6 +9,11 @@ resource "aws_instance" "firstec2" {
   instance_type = "t2.micro"
 }
 
+resource "aws_instance" "secondec2" {
+  ami     = "ami-0f36dcfcc94112ea1"
+  instance_type = "t2.micro"
+}
+
 resource "aws_vpc" "main" {
   cidr_block       = "${var.vpc_cidr}"
   instance_tenancy = "default"
